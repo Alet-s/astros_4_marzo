c4db71a82d6f4292e21d2c6b41fcb8e9f93a1b48
Alejandro Serrano <alexsr000@gmail.com>
2024-03-04 09:27:55 +0100

    Añadido método muestra.

--- src/astros_4_marzo/Astro.java
+++ src/astros_4_marzo/Astro.java
@@ -1,5 +1,14 @@
 package astros_4_marzo;
 
 public class Astro {
-
+	// Atributos de clase
+	String nombre;
+	double masa;
+	double tempMedia;
+	
+	public void muestra(){
+		System.out.println(nombre);
+		System.out.println(masa);
+		System.out.println(tempMedia);
+	}
 }
